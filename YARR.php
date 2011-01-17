3f0b50a76e95c0961592519d66152793b56e19e5
Toni Spets <toni.spets@iki.fi>
2011-01-17 19:50:46 +0200

    Add data types has_many, datetime and boolean, fix bugs everywhere

--- YARR.php
+++ YARR.php
@@ -41,6 +41,7 @@ abstract class YARR
                 $this->attributes[$k] = $data[$k];
             } else {
                 if (array_key_exists('default', $v)) {
+                    if (is_bool($v['default'])) $v['default'] = (int)$v['default']; // FIXME: generalize
                     $this->attributes[$k] = $v['default'];
                 } else {
                     /* even if not null is set, we default to null, saving or validating will just fail */
@@ -56,10 +57,15 @@ abstract class YARR
             return $this->errors;
         }
 
-        if(array_key_exists($k, $this->attributes))
+        if (array_key_exists($k, $this->attributes)) {
+            /* when getting a has_many relation, replace the internal integer value with an object, save will use it's id */
+            if (array_key_exists($k, static::$schema) && static::$schema[$k]['type'] == 'has_many') {
+                if ($this->attributes[$k] !== NULL && is_numeric($this->attributes[$k])) {
+                    $this->attributes[$k] = call_user_func(static::$schema[$k]['class'].'::find', 'first', array('id = ?', $this->attributes[$k]));
+                }
+            }
             return $this->attributes[$k];
-
-        return NULL;
+        }
     }
 
     function __set($k, $v)
@@ -68,6 +74,12 @@ abstract class YARR
         if (is_bool($v)) $v = (int)$v;
 
         if (array_key_exists($k, $this->attributes)) {
+            /* disallow saving a wrong object in has_many relation */
+            if (static::$schema[$k]['type'] == 'has_many') {
+                if (is_object($v) && get_class($v) != static::$schema[$k]['class']) {
+                    return false;
+                }
+            }
             $this->__dirty[$k] = true;
             return ($this->attributes[$k] = $v);
         }
@@ -90,6 +102,30 @@ abstract class YARR
         $this->attributes['id'] = NULL;
     }
 
+    function toArray()
+    {
+        /* FIXME: trigger has_many relations so the result will have objects instead of integers */
+        return (array)(clone (object)$this->attributes);
+    }
+
+    /**
+     * $keys = array of primary keys, 'all', 'first' or 'last'
+     * $options = (
+     *      'where'     => 'WHERE' or array('WHERE', bind, bind, ...),
+     *      'limit'     => int,
+     *      'offset'    => int,
+     *      'order'     => 'ORDER BY',
+     *      'select'    => 'SELECT ? FROM',
+     *      'from'      => 'a AS b',
+     *      'group      => 'GROUP BY',
+     *      'having'    => 'HAVING'
+     * )
+     */
+    function find($keys, $options)
+    {
+        return NULL;
+    }
+
     function validate()
     {
         $this->errors = array();
@@ -101,6 +137,18 @@ abstract class YARR
                         $this->errors[] = $k;
                     }
                     break;
+                case 'has_many':
+                    /* can't save a relation if the relation itself ain't saved first */
+                    if (is_object($this->attributes[$k])) {
+                        if ($this->attributes[$k]->id === NULL) {
+                            $this->errors[] = $k;
+                        }
+                    }
+                    else if (!preg_match('/^\d*$/', $this->attributes[$k])) {
+                        $this->errors[] = $k;
+                    }
+                    break;
+                case 'boolean':
                 case 'integer':
                     if (!preg_match('/^\d*$/', $this->attributes[$k])) {
                         $this->errors[] = $k;
@@ -108,6 +156,11 @@ abstract class YARR
                     break;
                 case 'decimal':
                     if (!preg_match('/^((\d*)|(\d+\.\d+))$/', $this->attributes[$k])) {
+                        $this->errors[] = $k;
+                    }
+                    break;
+                case 'datetime';
+                    if (!preg_match('/^(\d\d\d\d\-\d\d\-\d\d \d\d:\d\d:\d\d){0,1}$/', $this->attributes[$k])) {
                         $this->errors[] = $k;
                     }
                     break;
@@ -120,7 +173,8 @@ abstract class YARR
             }
 
             if (array_key_exists('unique', $data) && $data['unique']) {
-                /* TODO: find by this column to validate? */
+                if (!is_null(self::find('first', array('id = ?', $this->attributes[$k]))))
+                    $this->errors[] = $k;
             }
         }
 
@@ -159,12 +213,12 @@ abstract class YARR
         $data = array();
 
         foreach($this->attributes as $k => $v) {
-            if($k == 'id')
+            if ($k == 'id')
                 continue;
-            $data[self::quoteName($k)] = self::quote($v);
+            $data[self::quoteName($k)] = self::quote($k, $v);
         }
 
-        self::$db->exec('INSERT INTO '.self::quoteName($table).' ('.implode(array_keys($data), ',').') VALUES('.implode($data, ',').')');
+        self::$db->exec('INSERT INTO '.self::quoteName($table, true).' ('.implode(array_keys($data), ',').') VALUES('.implode($data, ',').')');
 
         $this->attributes['id'] = self::$db->lastInsertId();
     }
@@ -184,10 +238,10 @@ abstract class YARR
 
         $pairs = array();
         foreach(array_keys($this->__dirty) as $k) {
-            $pairs[] = self::quoteName($k).' = '.self::quote($this->attributes[$k]);
+            $pairs[] = self::quoteName($k).' = '.self::quote($k, $this->attributes[$k]);
         }
 
-        self::$db->exec('UPDATE '.self::quoteName($table).' SET '.implode($pairs, ',').' WHERE '.self::quoteName('id').' = '.self::quote($this->attributes['id']));
+        self::$db->exec('UPDATE '.self::quoteName($table, true).' SET '.implode($pairs, ',').' WHERE '.self::quoteName('id').' = '.self::quote($k, $this->attributes['id']));
     }
 
     static function init(PDO $db, $treat_empty_as_null = true)
@@ -199,16 +253,38 @@ abstract class YARR
         static::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     }
 
-    private static function quote($str)
+    private static function quote($key, $value)
     {
-        if (is_null($str) || (self::$treat_empty_as_null && strlen($str) == 0))
+        /* save has_many relations correctly */
+        if (static::$schema[$key]['type'] == 'has_many') {
+            if (is_object($value))
+                $value = $value->id;
+        }
+
+        /* integer values as integer */
+        if (preg_match('/^\d+\.{0,1}\d*$/', $value)) {
+            return (string)$value;
+        }
+
+        if (is_null($value) || (self::$treat_empty_as_null && strlen($value) == 0))
             return 'NULL';
 
-        return self::$db->quote($str);
+        return self::$db->quote($value);
     }
 
-    private static function quoteName($str)
+    private static function quoteName($str, $raw = false)
     {
+        /* has_many relation key handling */
+        if (!$raw) {
+            if (static::$schema[$str]['type'] == 'has_many') {
+            if ($str == 'host')
+                if (array_key_exists('key', static::$schema[$str])) {
+                    $str = static::$schema[$str];
+                } else {
+                    $str = $str.'_id';
+                }
+            }
+        }
         /* FIXME: backtick is not ANSI SQL (MySQL uses it, SQLite has compatibility) */
         return "`{$str}`";
     }
@@ -228,14 +304,15 @@ abstract class YARR
 
         ob_start();
 
-        echo "CREATE TABLE ".self::quoteName($table)." (\n";
-        echo "\t".self::quoteName('id')." INTEGER PRIMARY KEY";
+        echo "CREATE TABLE ".self::quoteName($table, true)." (\n";
+        echo "\t".self::quoteName('id', true)." INTEGER PRIMARY KEY";
 
         foreach(static::$schema as $name => $data) {
             if (!array_key_exists('type', $data))
                 throw new Exception('No type specified for column', self::E_NO_TYPE);
 
-            echo ",\n\t".self::quoteName($name);
+            echo ",\n\t";
+            echo self::quoteName($name);
 
             switch($data['type']) {
                 case 'string':
@@ -255,11 +332,20 @@ abstract class YARR
                         }
                     }
                     break;
+                case 'boolean':
+                    echo ' UNSIGNED INTEGER(1)';
+                    break;
                 case 'decimal':
                     echo ' DECIMAL';
-                    if(array_key_exists('size', $data)) {
+                    if (array_key_exists('size', $data)) {
                         echo "({$data['size']})";
                     }
+                    break;
+                case 'has_many':
+                    echo ' INTEGER';
+                    break;
+                case 'datetime':
+                    echo ' DATETIME';
                     break;
                 default:
                     throw new Exception("Unknown type '{$data['type']}'", self::E_UNK_TYPE);
@@ -282,7 +368,11 @@ abstract class YARR
                 if (is_null($data['default'])) {
                     echo ' NULL';
                 } else {
-                    echo ' '.static::$db->quote($data['default']);
+                    if ($data['type'] == 'boolean') {
+                        echo ' '.(int)$data['default'];
+                    } else {
+                        echo ' '.static::$db->quote($data['default']);
+                    }
                 }
             }
         }
